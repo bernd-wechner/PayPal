a229a372c6de6c59320bf457c9286b35586ebc99
Bernd Wechner <bernd.wechner@gmail.com>
2020-11-16 21:55:18 +1100

    A shot at including payer information
    Pretty louse SDK. Hard to use, and incomplete and dodgy info it provides. The addresses it returns are all over the shop. But this is a cut at providing some payer info in the report,

--- download_transactions.py
+++ download_transactions.py
@@ -46,7 +46,8 @@ else:
               "Balance", 
               "Num", 
               "Description", 
-              "Notes", 
+              "Notes",
+              "Partner", 
               "Transfer Account"]
     print(", ".join(header))        
     
@@ -64,6 +65,43 @@ else:
         fee = float(fee_amount.value) if fee_amount else 0
         fcurrency = t.transaction_info.fee_amount.currency_code if fee else ""
         
+        if amount > 0:
+            if hasattr(t, "payer_info"):
+                email = getattr(t.payer_info, "email_address", "")
+                name = getattr(t.payer_info.payer_name, "alternate_full_name", "")
+                account_id = getattr(t.payer_info, "account_id", "")
+                
+#                 if not email and not name:
+#                     breakpoint()
+                
+                address = ""
+                if hasattr(t.payer_info, "address"):
+                    addr = [
+                        getattr(t.payer_info.address, "line1", "").replace("No Street Address Provided", ""),
+                        getattr(t.payer_info.address, "city", "").replace("No City Provided", ""),
+                        getattr(t.payer_info.address, "postal_code", ""),
+                        getattr(t.payer_info.address, "state", ""),
+                        getattr(t.payer_info.address, "country_code", "")
+                        ]
+                    
+                    address = ". ".join(addr)
+                
+                partner = account_id
+                partner += f" {name}" if name else ""
+                partner += f" <{email}>" if email else ""
+                partner = partner.strip()
+                
+                # The addresses the paypal SDK returns are 
+                # complete nonsense. There's bug either at 
+                # the server end or in the Python libs, but 
+                # being a v1 API they claim no support for 
+                # it any more. 
+                #partner += " " + address.strip()
+            else:
+                partner = "unknown payer"
+        else:
+            partner = "unknown recipient"
+            
         # The PayPal Commission on the transaction
         # cart items don't know this so we have to 
         # work it out at the transaction level and 
@@ -76,34 +114,30 @@ else:
         if hasattr(cart, "item_details"):
             wait_for_amount = False
             for item in cart.item_details:
-                try:
-                    name = getattr(item, "item_name", "")
-                    if wait_for_amount:
-                        if not name == "Amount":
-                            raise Exception("Oops: Expecting an Amount item following a transaction without an amount and didn't find one.")                    
-                    else:    
-                        quantity = item.item_quantity
-    
-                    # PayPal transaction exhibit one gran weirdness at least
-                    # Typically each item in the cart as an item_amount 
-                    # BUT sometimes this is missing. if that's the case 
-                    # the next item in the cart will be named "Amount"
-                    # and provide the amount. Go figure.                  
-                    if hasattr(item, "item_amount"):
-                        amount = float(item.item_amount.value)
-                        deposit = amount * (1+pp_commission)
-                        currency = item.item_amount.currency_code
-                        wait_for_amount = False
-                    elif wait_for_amount:
-                        # If w're waiting for an amount, we should have
-                        # got one. It's a problem if not.
-                        raise Exception("Oops: Was waiting for an Amount item, it came, but didn't actually contain an amount!")                    
-                    else:
-                        wait_for_amount = True
-                        continue
-                        
-                except Exception as E:
-                    breakpoint()
+                item_name = getattr(item, "item_name", "")
+                if wait_for_amount:
+                    if not item_name == "Amount":
+                        raise Exception("Oops: Expecting an Amount item following a transaction without an amount and didn't find one.")                    
+                else:    
+                    quantity = item.item_quantity
+
+                # PayPal transaction exhibit one gran weirdness at least
+                # Typically each item in the cart as an item_amount 
+                # BUT sometimes this is missing. if that's the case 
+                # the next item in the cart will be named "Amount"
+                # and provide the amount. Go figure.                  
+                if hasattr(item, "item_amount"):
+                    amount = float(item.item_amount.value)
+                    deposit = amount * (1+pp_commission)
+                    currency = item.item_amount.currency_code
+                    wait_for_amount = False
+                elif wait_for_amount:
+                    # If w're waiting for an amount, we should have
+                    # got one. It's a problem if not.
+                    raise Exception("Oops: Was waiting for an Amount item, it came, but didn't actually contain an amount!")                    
+                else:
+                    wait_for_amount = True
+                    continue
     
                 summary = [idate, 
                           f"CURRENCY::{currency}", 
@@ -112,8 +146,9 @@ else:
                           quantity, 
                           f"{balance:.2f}", 
                           id, 
-                          name.strip(), 
-                          note.strip()]
+                          item_name.strip(), 
+                          note.strip(),
+                          partner]
                 
                 print(",".join(summary))        
         else:
@@ -125,5 +160,6 @@ else:
                       f"{balance:.2f}", 
                       id, 
                       "", 
-                      note.strip()]
+                      note.strip(),
+                      partner]
             print(",".join(summary))        
